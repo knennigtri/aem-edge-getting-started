59e4a828ef666bebee0da8997b2a2bfde4022229
Kevin Nennig <knennig213@gmail.com>
2025-07-30 14:51:48 -0400

    switched back to dev

--- api/aem-gql-connection.js
+++ api/aem-gql-connection.js
@@ -1,9 +1,9 @@
 // Dynamic switching for Universal Editor (author) and .aem.page/aem.live (publish)
 function getAEMHost() {
   if (window.location.hostname.endsWith('adobeaemcloud.com')) {
-    return 'https://author-p156903-e1664873.adobeaemcloud.com';
+    return 'https://author-p156903-e1726641.adobeaemcloud.com';
   }
-  return 'https://publish-p156903-e1664873.adobeaemcloud.com';
+  return 'https://publish-p156903-e1726641.adobeaemcloud.com';
 }
 
 function getCDNCacheBuster() {
